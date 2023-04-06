9ac372f65f11c207f603c256bd7f1d99677a33c9
Serhey <kovalo699@gmail.com>
2023-04-06 11:35:34 +0200

    Вроде бы всё

---
+++ config.py
@@ -0,0 +1,6 @@
+TOKEN = '5706880498:AAHvpbJr9eYMbut2MloyGgscN4TBrkPvkCs'
+keys = {'рубыль':               'RUB',
+        'доллар':               'USD',
+        'евро':                 'EUR',
+        'гривна':               'UAH',
+        'чешскаякрона':         'CZK'}
